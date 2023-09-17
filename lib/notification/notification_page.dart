84c01d8389efec3d1a1e9844afc42037b2369193
yusrilhasriansyah@gmail.com <yusrilhasriansyah@gmail.com>
2023-09-17 16:48:08 +0700

    AAT-82 & AAT-83

--- lib/notification/notification_page.dart
+++ lib/notification/notification_page.dart
@@ -1,3 +1,5 @@
+import 'package:alumni_forum_app/helper/custom_theme.dart';
+import 'package:alumni_forum_app/notification/card_notification.dart';
 import 'package:flutter/material.dart';
 
 class NotificationPage extends StatefulWidget {
@@ -10,13 +12,53 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("Notification Page")],
-        ),
+    return Container(
+      color: grayColorTheme,
+      child: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20.0,
+              top: 20.0,
+            ),
+            child: Text(
+              "Hari Ini",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          // Card follow user
+          CardNotification(cardCategory: 1),
+          // Card join forum
+          CardNotification(cardCategory: 2),
+          // Card liked post
+          CardNotification(cardCategory: 3),
+          // Card comment post
+          CardNotification(cardCategory: 4),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20.0,
+              top: 40.0,
+            ),
+            child: Text(
+              "Kemarin",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          // Card follow user
+          CardNotification(cardCategory: 1),
+          // Card join forum
+          CardNotification(cardCategory: 2),
+          // Card liked post
+          CardNotification(cardCategory: 3),
+          // Card comment post
+          CardNotification(cardCategory: 4),
+        ],
       ),
     );
   }
--- lib/search/search_page.dart
+++ lib/search/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:alumni_forum_app/helper/custom_theme.dart';
 import 'package:alumni_forum_app/search/card_search_default.dart';
+import 'package:alumni_forum_app/search/top_search.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
@@ -20,9 +21,11 @@ class _SearchPageState extends State<SearchPage> with TickerProviderStateMixin {
 
     // listen to focus changes
     widget.searchNode.addListener(() {
-      setState(
-        () {},
-      ); //setState declared to make the widget loaded each time the textformfield tapped (has focus/focus on)
+      if (mounted) {
+        setState(
+          () {},
+        ); //setState declared to make the widget loaded each time the textformfield tapped (has focus/focus on)
+      }
       print(
         'focusNode updated: hasFocus: ${widget.searchNode.hasFocus}',
       );
@@ -37,13 +40,13 @@ class _SearchPageState extends State<SearchPage> with TickerProviderStateMixin {
         Center(
           child: (widget.searchNode.hasFocus)
               ? Container(
-                  color: silverColorTheme,
+                  color: grayColorTheme,
                   child: TopSearch(
                     isUser: true,
                   ),
                 )
               : Container(
-                  color: silverColorTheme,
+                  color: grayColorTheme,
                   child: ListView.builder(
                     itemBuilder: (context, index) {
                       return CardSearch(
@@ -58,13 +61,13 @@ class _SearchPageState extends State<SearchPage> with TickerProviderStateMixin {
         Center(
           child: (widget.searchNode.hasFocus)
               ? Container(
-                  color: silverColorTheme,
+                  color: grayColorTheme,
                   child: TopSearch(
                     isUser: false,
                   ),
                 )
               : Container(
-                  color: silverColorTheme,
+                  color: grayColorTheme,
                   child: ListView.builder(
                     itemBuilder: (context, index) {
                       return CardSearch(
@@ -75,75 +78,6 @@ class _SearchPageState extends State<SearchPage> with TickerProviderStateMixin {
                     itemCount: 10,
                   ),
                 ),
-        ),
-      ],
-    );
-  }
-}
-
-class TopSearch extends StatelessWidget {
-  bool isUser;
-  TopSearch({super.key, required this.isUser});
-
-  List<Widget> generateCardSearchWidgets(int count, {bool isUser = true}) {
-    List<Widget> cardSearchWidgets = [];
-    for (int i = 0; i < count; i++) {
-      cardSearchWidgets.add(
-        CardSearch(
-          userOrForumName: "${isUser ? 'User' : 'Forum'} ke-${i + 1}",
-          isUser: isUser,
-        ),
-      );
-    }
-    return cardSearchWidgets;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 30),
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            left: 20,
-          ),
-          child: Text(
-            "Pencarian teratas",
-            style: TextStyle(
-                color: darkRedColorTheme,
-                fontWeight: FontWeight.w700,
-                fontSize: 18),
-          ),
-        ),
-        ...generateCardSearchWidgets(5, isUser: isUser),
-        Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
-            top: 20,
-          ),
-          child: Center(
-            child: InkWell(
-              onTap: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text(
-                      "Tampilkan lebih banyak text clicked!",
-                    ),
-                    duration: Duration(
-                      milliseconds: 1250,
-                    ),
-                  ),
-                );
-              },
-              child: Text(
-                "Tampilkan lebih banyak",
-                style: TextStyle(
-                    color: darkRedColorTheme,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 18),
-              ),
-            ),
-          ),
         ),
       ],
     );
